eced9e624227834a7c9f39b0fb450a67adeb2cc6
alsoshubham <shubhamsahu100100@hotmail.com>
2024-10-08 18:41:02 +0530

    putting on hold

--- src/components/Inputbox.jsx
+++ src/components/Inputbox.jsx
@@ -1,4 +1,3 @@
-
 function InputBox({
     label,
     amount,
@@ -7,10 +6,8 @@ function InputBox({
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
-    currencyOption = []
+    currencyOptions = []
 
-
-    
     className = "",
 }) {
    
@@ -26,18 +23,21 @@ function InputBox({
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e)=>onAmountChange && onAmountChange(NUmber(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    
+                    onChange={(e)=> onAmountChange && onAmountChange(e.target.value)}
+                    disabled={currencyDisable}
                 >
-                    
-                        <option value="usd">
+                    {currencyOptions.map((currency)=>(<option value="usd">
                             usd
-                        </option>
+                        </option>))}
                 
                 </select>
             </div>
